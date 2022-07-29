285cdf6ab8d18c88f7beecaa5f3ac11bb68b2861
Milan Bačkonja <milan.backonja@hotmail.com>
2022-07-29 11:49:06 +0200

    Resource should not be string type

--- src/Events/ButtonClick.php
+++ src/Events/ButtonClick.php
@@ -8,11 +8,11 @@ class ButtonClick
 {
     use Dispatchable;
 
-    public string $resource;
+    public $resource;
 
     public ?string $key;
 
-    public function __construct(string $resource, ?string $key)
+    public function __construct($resource, ?string $key)
     {
         $this->resource = $resource;
         $this->key = $key;
